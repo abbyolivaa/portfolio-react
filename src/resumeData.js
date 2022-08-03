0883c4a54b3941ee982d8a93b534ae3cc12da551
Abby Oliva <abbyoliva6@icloud.com>
2022-08-02 22:31:48 -0400

    resume data

---
+++ src/resumeData.js
@@ -0,0 +1,93 @@
+let resumeData = {
+    "imagebaseurl":"https://abbyolivaa.github.io/",
+    "name": "Abigail Oliva",
+    "role": "Full Stack Developer",
+    "linkedinId":"Your LinkedIn ID",
+    "roleDescription": "My career helps me live my dream life while nerding out at home with my computer. I love traveling and creating content online on various platforms like YouTube, Instagram, and Tiktok. I thuroughly enjoy learning new technologies and languages.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/abigail-oliva-920604229",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/abbyolivaa",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently a graduate from Columbia University's Computer Science, Coding Bootcamp. This year I plan to enroll in Harvard's coding program to better my computer science skill set to improve my work ethic in the field.",
+    "address":"New York, NY",
+    "website":"https://abbyolivaa.github.io",
+    "education":[
+      {
+        "UniversityName":"Columbia University",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2022",
+        "Achievements":"Some Achievements"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Etsy",
+        "specialization":"Upsale",
+        "MonthOfLeaving":"currently ",
+        "YearOfLeaving":"employeed",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"YouTube",
+        "specialization":"Content Creator",
+        "MonthOfLeaving":"currently ",
+        "YearOfLeaving":"employeed",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"MYSQL"
+      },
+      {
+        "skillname":"Git"
+      },
+      {
+        "skillname":"Expressjs"
+      }
+    ],
+    //
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ]
+  }
+  
+  export default resumeData
