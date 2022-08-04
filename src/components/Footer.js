98f6c2c3126e72a276190bbc75fedf30a8bbe86e
Abby Oliva <abbyoliva6@icloud.com>
2022-08-03 23:29:46 -0400

    final commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,12 @@
 import React from "react";
-//import icons from fontawesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// Github
-
-//LinkedIn
-//import {faGithubAlt} from '@fortawesome/free-solid-svg-icons'
-//Twitter
-//import {faGithubAlt} from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
 
 
   return (
+
     <footer className="flex-row px-1 justify-content-center">
+      <p>My info:</p>
       <ul className="flex-row">
         <li className="mx-2">
           <a href="https://github.com/abbyolivaa">
--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,19 +1,66 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`Text required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
   return (
-    <section>
-      <h2 id="about" className="d-flex justify-content-center ">Contact Us</h2>
-      <br></br>
-      <div className="d-flex justify-content-center ">
-         <ul>
-             <li><a href=''>Email</a></li>
-             <li><a href='https://www.linkedin.com/in/abigail-oliva-920604229'>LinkedIn</a></li>
-             <li><a href='tel:+5555555555'>555-555-5555</a></li>
-         </ul>
-      </div>
+    <section className='contactItems'>
+      <h2 id="about" className="d-flex justify-content-center">Contact me</h2>
+      <form id="contact-form" className='form d-flex align-items-start justify-content-center' onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input className='mx-1' type="text" name="name" defaultValue="Name" onBlur={handleChange} />
+        </div>
+        <br></br>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input className='mx-1 ' type="email" name="email" defaultValue="Email" onBlur={handleChange} />
+        </div>
+        <br></br>
+        <div className="mb-3 d-flex position-relative">
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" className='mx-1 ' rows="5" defaultValue="Message" onBlur={handleChange} />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <br></br>
+        <button data-testid="button" type="submit">Submit</button>
+      </form>
     </section>
-    
   );
 }
 export default Contact;
